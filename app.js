7cfad5ee3b0f804afaf9459a92068be7ee7611be
AlanSalviano <alanqms@gmail.com>
2024-09-06 19:17:29 -0300

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa");
 
+    let section = document.getElementById
+    ("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    //optei por colocar para aparecer todo resultado de pesquisa ao clicar pesquisar
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
 
     for (let dado of dados) {
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            //cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a target="_blank">${dado.titulo}</a>
@@ -13,6 +25,7 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Área do Doador</a>
             </div>
         `;
+        }
     }
     section.innerHTML = resultados;
 }
